de0adf33289de05c1b13e2f246f5fcfd2da6f0d2
Giangblackk <giangblackk@gmail.com>
2017-03-07 17:43:53 +0700

    [add] simple image process task

--- hellocelery/celeryapp/tasks.py
+++ hellocelery/celeryapp/tasks.py
@@ -1,7 +1,7 @@
 from celery import shared_task, current_task
 from numpy import random
 from scipy.fftpack import fft
-
+from skimage import io
 @shared_task
 def fft_random(n):
     for i in range(n):
@@ -15,3 +15,9 @@ def fft_random(n):
 @shared_task
 def say_hello(n):
     return n
+@shared_task
+def simple_image_process(file_name):
+    random_number = random.randint(1,100)
+    image = io.imread(file_name,as_grey=True)
+    io.imsave(file_name + str(random_number) +'.png',image)
+    return random_number
--- hellocelery/celeryapp/views.py
+++ hellocelery/celeryapp/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from tasks import fft_random
+from tasks import fft_random, simple_image_process
 def index(request):
-    x = fft_random.delay(1000000)
-    print(x.status)
+    # x = fft_random.delay(1000000)
+    # print(x.status)
     # print(x.get())
+    x = simple_image_process.delay('/home/giangblackk/Desktop/eyes.jpg')
+    # x = simple_image_process.delay('./eyes.jpg')
+    print(x.status)
     return HttpResponse("Hello, world. You're at the polls index.")
