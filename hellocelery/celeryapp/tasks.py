64631a663b8dc0de5c2fdf21063292a0bd8389eb
Giangblackk <giangblackk@gmail.com>
2017-03-08 16:21:54 +0700

    [update] remove failed NotifierTask specify sender

--- hellocelery/celeryapp/tasks.py
+++ hellocelery/celeryapp/tasks.py
@@ -6,26 +6,7 @@ from hellocelery.celery import app
 import requests
 from celery.signals import task_success
 
-@task_success.connect
-def task_sent_handler(sender=None, headers=None, body=None, **kwargs):
-    # information about task are located in headers for task messages
-    # using the task protocol version 2.
-    print('Task Success')
-    url = 'http://localhost:8000'
-    requests.post(url)
 
-
-class NotifierTask(Task):
-    """Task that sends notification on completion."""
-    # def after_return(self, status, retval, task_id, args, kwargs, einfo):
-    def on_success(self, retval, task_id, args, kwargs):
-        print('After Return')
-        url = 'http://localhost:8000'
-        # data = {'clientid': kwargs['clientid'], 'result': retval}
-        requests.post(url)
-
-
-@app.task(base=NotifierTask)
 @shared_task
 def fft_random(n):
     for i in range(n):
@@ -49,3 +30,25 @@ def simple_image_process(file_name):
     image = io.imread(file_name,as_grey=True)
     io.imsave(file_name + str(random_number) +'.png',image)
     return random_number
+
+@shared_task
+def test(tid, n):
+    for i in range(n):
+        x = random.normal(0, 0.1, 2000)
+        y = fft(x)
+        if(i%30 == 0):
+            process_percent = int(100 * float(i)/float(n))
+            current_task.update_state(state='PROGRESS',
+                meta={'process_percent': process_percent})
+    return random.random()
+
+
+# @task_success.connect(sender='celeryapp.tasks.fft_random')
+@task_success.connect(sender=fft_random)
+def task_id_sent_handler(sender=None, headers=None, body=None, **kwargs):
+    # information about task are located in headers for task messages
+    # using the task protocol version 2.
+    print(sender)
+    print('\n ***Task Success***')
+    url = 'http://localhost:8000'
+    requests.post(url)
